bf89b40a5e54dc8891108b33439b96264ca1bffb
zhenjie hao <zhenjieh@umich.edu>
2018-06-08 18:45:17 -0400

    improved the UX performance

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,25 @@
 $(document).ready(function(){
-    $("#upload").click(function(e){
-        e.preventdDefault();
-        $.post("/newword",
-        {
-        word:'helo',
-        mean:'world',
-        note:'wow'
-        },
-        function(data, status){
-            alert("Data: " + data + "\nStatus: " + status);
+ 
+    $("#upload").click(function(e){       
+        e.preventDefault();
+        $.ajax({
+            url:'/newword',
+            type:'post',
+            data:$('#uploadForm').serialize(),
+            success:function(){
+                swal({
+                    position: 'top-end',
+                    type: 'success',
+                    title: 'You have successfully uploaded the word!',
+                    showConfirmButton: false,
+                    timer: 1500 
+                  }).then(function(){
+                      window.location.href = '/';
+                  }, function(){
+                    console.log("Oh, upload failed..."); 
+                  });
+            }
         });
     });
+
 });
