df1698d2874d2f7f5e2684ed56f1a8e69a9c7b93
zhenjie hao <zhenjieh@umich.edu>
2018-06-11 18:30:15 -0400

    achieve the function of text to speech

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
- 
+    
+    $("i.material-icons").click(function(){
+        // console.log($(this).closest("div.card").siblings("h3").html());
+        var msg = new SpeechSynthesisUtterance($(this).closest("div.card").siblings("h3").html());
+        window.speechSynthesis.speak(msg)
+    });
+
     $("#upload").click(function(e){       
         e.preventDefault();
         $.ajax({
